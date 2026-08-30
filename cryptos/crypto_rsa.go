b6294a65ff01fcfa5b1db6b0ca62f1ed65fd16e1
dev-anego-linux <>
2026-08-30 13:47:56 +0900

    処理を修正

--- cryptos/crypto_rsa.go
+++ cryptos/crypto_rsa.go
@@ -4,6 +4,7 @@ import (
 	"crypto/rand"
 	"crypto/rsa"
 	"crypto/sha256"
+	"encoding/base64"
 	"fmt"
 )
 
@@ -14,11 +15,17 @@ type RSAKey struct {
 
 var key = &RSAKey{}
 
-func Encrypt(plainText string) ([]byte, error) {
+type RSAService struct{}
+
+func NewRSAService()(*RSAService){
+	return &RSAService{}
+}
+
+func (r *RSAService) Encrypt(plainText string) (string, error) {
 	privateKey, err := rsa.GenerateKey(rand.Reader, 4096)
 	if err != nil {
 		fmt.Println("秘密鍵の生成に失敗しました")
-		return nil, err
+		return "", err
 	}
 
 	key.privateKey = privateKey
@@ -26,16 +33,17 @@ func Encrypt(plainText string) ([]byte, error) {
 	cipherText, err := rsa.EncryptOAEP(sha256.New(), rand.Reader, key.publicKey, []byte(plainText), nil)
 	if err != nil {
 		fmt.Println("暗号化に失敗しました")
-		return nil, err
+		return "", err
 	}
-	return cipherText, nil
+	return base64.StdEncoding.EncodeToString(cipherText), nil
 }
 
-func Decrypt(cipherText string) ([]byte, error) {
-	plainText, err := rsa.DecryptOAEP(sha256.New(), rand.Reader, key.privateKey, []byte(cipherText), nil)
+func (r *RSAService) Decrypt(cipherText string) (string, error) {
+	decodedText, _ := base64.StdEncoding.DecodeString(cipherText)
+	plainText, err := rsa.DecryptOAEP(sha256.New(), rand.Reader, key.privateKey, decodedText, nil)
 	if err != nil {
 		fmt.Println("復号に失敗しました")
-		return nil, err
+		return "", err
 	}
-	return plainText, nil
+	return string(plainText), nil
 }
